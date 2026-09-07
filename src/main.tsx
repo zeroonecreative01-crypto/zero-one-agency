b30da7bf16d56716fdd61347a7b6bb4c3f4cabc1
zeroonecreative01-crypto <zeroone.creative.01@gmail.com>
2026-09-07 19:12:34 +0300

    feat: connect public portfolio to CMS

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AdminRoute, AuthProvider } from './auth/AuthProvider';
 import AdminAccessButton from './components/AdminAccessButton';
+import RemotePortfolioSync from './components/RemotePortfolioSync';
 import './lib/leadsCapture';
 import './styles.css';
 import './site-polish.css';
@@ -21,6 +22,7 @@ function Root() {
   return (
     <>
       <App />
+      <RemotePortfolioSync />
       <AdminAccessButton />
     </>
   );
