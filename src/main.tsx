cf29e0133d9a4d264356036313eb45e0233614b3
zeroonecreative01-crypto <zeroone.creative.01@gmail.com>
2026-09-07 17:07:56 +0300

    feat: mount authentication provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from './auth/AuthProvider';
 import './styles.css';
 import './site-polish.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 );
