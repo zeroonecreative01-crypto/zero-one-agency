9a4bd9ce274f7935c08c6b25bc98741cdceafe3f
zeroonecreative01-crypto <zeroone.creative.01@gmail.com>
2026-09-07 04:29:33 +0300

    Load final site polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './site-polish.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
