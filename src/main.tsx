cca4cf3c8cf1b2739b9d07b704a61556aafefb73
zeroonecreative01-crypto <zeroone.creative.01@gmail.com>
2026-09-07 23:25:46 +0300

    Revert temporary online iPhone mockup

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,6 @@ import './site-polish.css';
 import './pricing-polish.css';
 import './components/FloatingIsland.css';
 import './components/FloatingIslandFix.css';
-import './components/RealIPhoneMockup.css';
 
 function Root() {
   const [path, setPath] = useState(() => window.location.pathname.replace(/\/+$/, '') || '/');
