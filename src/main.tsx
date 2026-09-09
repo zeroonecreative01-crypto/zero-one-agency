3180585a5590fc7ee2a006357003d2f2a65eeadb
zeroonecreative01-crypto <zeroone.creative.01@gmail.com>
2026-09-09 04:55:31 +0300

    fix: show custom package builder only on home

--- src/main.tsx
+++ src/main.tsx
@@ -39,7 +39,7 @@ function Root() {
       <FloatingIsland />
       <RemotePortfolioSync />
       <RemotePricingSync />
-      <CustomPackageBuilder />
+      {path === '/' && <CustomPackageBuilder />}
       <SupportCenter />
       <SiteContactPatch />
       <ExperienceUpgrade />
